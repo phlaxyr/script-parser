7a59eb099a561aae3f51bdfa3e230a16d0675c31
minerguy31 <minerguy31@gmail.com>
2016-07-09 17:54:30 -0600

    Add Expressions
    Boilerplate for Expression parser

---
+++ src/script/parser/expressions/Expression.java
@@ -0,0 +1,9 @@
+package script.parser.expressions;
+
+import java.io.Serializable;
+
+public abstract class Expression implements Serializable {
+	
+	private static final long serialVersionUID = 8601239840329721033L;
+	
+}
