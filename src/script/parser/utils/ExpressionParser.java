b821f89f178a71ef703eba5a9b4f2dcbc3ada310
BuildTools <unconfigured@null.spigotmc.org>
2016-07-09 21:59:49 -0500

    WOOOOOOOOOOOOOOOOORDS

--- src/script/parser/utils/ExpressionParser.java
+++ src/script/parser/utils/ExpressionParser.java
@@ -1,10 +1,12 @@
 package script.parser.utils;
 
+import script.parser.expressions.BasicExpression;
+import script.parser.expressions.BlockExpression;
 import script.parser.expressions.Expression;
 
 public final class ExpressionParser {
 	private ExpressionParser() {}
-	private static final String[] NUMBEROPERATORS = {"+","-","*","/","^","=","("};
+	private static final String[] NUMBEROPERATORS = {"\\+","-","\\*","/","^","=","\\("};
 	
 	/*
 	 * To fully parse-ify text, use a combination of hasExpressions and parseLevel().
@@ -19,27 +21,15 @@ public final class ExpressionParser {
 	 * 
 	 * 
 	 */
-	public static Expression parseLevel(String rawText) {
-		String leftVal;
-		String rightVal;
-		String operator;
-		String[] subRawText;
-		for(String str:NUMBEROPERATORS) {
-			if (rawText.contains(str)) {
-				operator=str;
-				subRawText=rawText.split(str);
-				leftVal=subRawText[0];
-				rightVal=subRawText[1];
-			}
-		}
-		
-	}
 	
 	public static Expression parse(String rawText) {
 		String leftVal;
 		String rightVal;
+		Expression leftExp = null;
+		Expression rightExp = null;
 		String operator;
 		String[] subRawText;
+		boolean hasExp=false;
 		for(String str:NUMBEROPERATORS) {
 			if (rawText.contains(str)) {
 				operator=str;
@@ -47,13 +37,18 @@ public final class ExpressionParser {
 				leftVal=subRawText[0];
 				rightVal=subRawText[1];
 				if(hasExpression(leftVal)) {
-					parse(leftVal);
+					hasExp=true;
+					leftExp = parse(leftVal);
 				}
 				if(hasExpression(rightVal)) {
-					parse(rightVal);
+					hasExp=true;
+					rightExp = parse(rightVal);
 				}
+				if(hasExp){
+					return new BlockExpression(leftExp,operator,rightExp);
+				} else return new BasicExpression(leftVal,operator,rightVal);
 			}
-		}
+		} return new BasicExpression(rawText,"","");
 		
 	}
 	
