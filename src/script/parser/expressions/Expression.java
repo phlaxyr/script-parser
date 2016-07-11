689f62929a7cf78f99bc5f343494cc328640ef05
minerguy31 <minerguy31@gmail.com>
2016-07-11 11:28:18 -0600

    Minor progress on getFromString

--- src/script/parser/expressions/Expression.java
+++ src/script/parser/expressions/Expression.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import script.parser.utils.ExpressionParser;
 
@@ -30,10 +31,22 @@ public abstract class Expression implements Serializable {
 	@Deprecated
 	public abstract void clearPlaceholders(Map<Integer, String> repl);
 	
+	private static String getSplitRegex(){
+		String[]regexops = new String[OPERATORS.length];
+		for(int i=0;i<OPERATORS.length;i++){
+			regexops[i] = Pattern.quote(OPERATORS[i]);
+		}
+		return "("+String.join("|", regexops)+")";
+	}
+	
 	public static Expression getFromString(String s){
 		//TODO: Get ops and vals
-		ArrayList<Operator> ops;
-		ArrayList<String> vals;
+		ArrayList<Operator> ops = new ArrayList<>();
+		
+		ArrayList<String> vals = new ArrayList<>();
+		String[]vs=s.replaceAll("(\\(|\\))", "").split(getSplitRegex());
+		System.out.println(new ArrayList<String>(Arrays.asList(vs)));
+		return null;
 	}
 	
 	private static boolean isOperator(String c){
