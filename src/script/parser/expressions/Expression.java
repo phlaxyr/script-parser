eacf8ba995375bad155772f6b8bf2f1249404edc
minerguy31 <minerguy31@gmail.com>
2016-07-11 10:54:11 -0600

    Added stubs for new Expression parsing

--- src/script/parser/expressions/Expression.java
+++ src/script/parser/expressions/Expression.java
@@ -2,9 +2,13 @@ package script.parser.expressions;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 
+import script.parser.utils.ExpressionParser;
+
 public abstract class Expression implements Serializable {
+	private static final String[] OPERATORS = {"+","-","*","/","^"};
 	
 	private static final long serialVersionUID = 8601239840329721033L;
 
@@ -25,4 +29,17 @@ public abstract class Expression implements Serializable {
 
 	@Deprecated
 	public abstract void clearPlaceholders(Map<Integer, String> repl);
+	
+	public static Expression getFromString(String s){
+		//TODO: Get ops and vals
+		ArrayList<Operator> ops;
+		ArrayList<String> vals;
+	}
+	
+	private static boolean isOperator(String c){
+		for(String d : OPERATORS)
+			if(c.equals(d))
+				return true;
+		return false;
+	}
 }
