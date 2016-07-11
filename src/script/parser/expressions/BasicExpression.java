0f9bc61de15bd1f0cfb89deba78f7702f91e9419
minerguy31 <minerguy31@gmail.com>
2016-07-11 09:48:00 -0600

    Deprecate old methods and constructors
    In preparation for rewrite of ExpressionParser

--- src/script/parser/expressions/BasicExpression.java
+++ src/script/parser/expressions/BasicExpression.java
@@ -1,5 +1,6 @@
 package script.parser.expressions;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import script.parser.utils.ExpressionParser;
@@ -8,17 +9,24 @@ public class BasicExpression extends Expression {
 
 	private static final long serialVersionUID = 5994162608353948937L;
 	
-	public String lval, op, rval;
-	
-	public BasicExpression(String lval, String op, String rval){
-		this.lval = lval;
-		this.op = op;
-		this.rval = rval;
-	}
+	public String op;
+	public ArrayList<String>values=new ArrayList<>();
 
+	public BasicExpression(String lval, String op, String rval){
+		this.values.add(lval);
+		this.op = op;
+		this.values.add(rval);
+	}
+	
+	public BasicExpression(ArrayList<String>val, String op){
+		this.values=val;
+		this.op = op;
+	}
+	
+	@Deprecated
 	@Override
 	public String getLval() {
-		return lval;
+		return values.get(0);
 	}
 
 	@Override
@@ -26,22 +34,28 @@ public class BasicExpression extends Expression {
 		return op;
 	}
 
+	@Deprecated
 	@Override
 	public String getRval() {
-		return rval;
+		return values.get(1);
 	}
 
+	@Deprecated
 	@Override
 	public void clearPlaceholders(Map<Integer,String> placeholders) {
 		while(this.getLval().contains(ExpressionParser.placeholder)){
-			int num = Integer.valueOf(lval.split("\\.")[1].replaceAll("\\D", ""));
-			lval = lval.replace(ExpressionParser.placeholder+num,placeholders.get(num));
-			System.out.println("LV "+lval);
+			int num = Integer.valueOf(values.get(0).split("\\.")[1].replaceAll("\\D", ""));
+			values.set(0,values.get(0).replace(ExpressionParser.placeholder+num,placeholders.get(num)));
 		}
 		while(this.getRval().contains(ExpressionParser.placeholder)){
-			int num = Integer.valueOf(rval.split("\\.")[1].replaceAll("\\D", ""));
-			rval = rval.replace(ExpressionParser.placeholder+num,placeholders.get(num));
+			int num = Integer.valueOf(values.get(1).split("\\.")[1].replaceAll("\\D", ""));
+			values.set(1,values.get(1).replace(ExpressionParser.placeholder+num,placeholders.get(num)));
 		}
+	}
+
+	@Override
+	public ArrayList<String> getVals() {
+		return values;
 	}
 
 
